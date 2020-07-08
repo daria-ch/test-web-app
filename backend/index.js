6856a69305233a6c143ec65e5e5c3cf32fe15df6
Daria Chernovol <fabulus.kg.dk@gmail.com>
2020-07-08 19:51:44 +0600

    Added database configuration

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+// const users = require('./app/users');
+// const categories = require('./app/categories');
+// const articles = require('./app/articles');
+
+const mysqlDb = require('./mysqlDb');
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+// app.use('/users', users);
+// app.use('/categories', categories);
+// app.use('/articles', articles);
+
+const run = async () => {
+
+    await mysqlDb.connect();
+
+    app.listen(port, () => {
+        console.log(`HTTP Server started on ${port} port!`);
+    });
+
+    process.on('exit', function () {
+        mysqlDb.disconnect();
+    });
+
+};
+
+run().catch(e => {
+    console.error(e);
+});
