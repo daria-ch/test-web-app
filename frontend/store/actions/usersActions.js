495b73af310612bd8b7a96bb1c6e8836b713cd41
Daria Chernovol <fabulus.kg.dk@gmail.com>
2020-07-10 15:54:01 +0600

    Added users actions and reducer

---
+++ frontend/store/actions/usersActions.js
@@ -0,0 +1,63 @@
+import {
+    EDIT_USER_SUCCESS,
+    FETCH_SINGLE_USER_SUCCESS,
+    FETCH_USERS_FAILURE, FETCH_USERS_REQUEST,
+    FETCH_USERS_SUCCESS,
+    POST_USER_SUCCESS
+} from "./actionTypes";
+import axiosApi from "../../axiosApi";
+
+
+export const fetchUsersSuccess = users => ({type: FETCH_USERS_SUCCESS, users});
+export const postUserSuccess = () => ({type: POST_USER_SUCCESS});
+export const editUserSuccess = (id, newUser) => ({type: EDIT_USER_SUCCESS, id, newUser});
+export const fetchSingleUserSuccess = user => ({type: FETCH_SINGLE_USER_SUCCESS, user});
+export const fetchUsersFailure = error => ({type: FETCH_USERS_FAILURE, error});
+export const fetchUsersRequest = () => ({type: FETCH_USERS_REQUEST});
+
+export const fetchUsers = () => {
+    return async dispatch => {
+        const response = await axiosApi.get('/users');
+        dispatch(fetchUsersSuccess(response.data));
+    }
+};
+
+export const postUser = user => {
+    return async dispatch => {
+        await axiosApi.post('/users', user);
+        dispatch(postUserSuccess());
+    }
+};
+
+export const fetchSingleUser = id => {
+    return async (dispatch) => {
+        const response = await axiosApi.get('/users/' + id);
+        dispatch(fetchSingleUserSuccess(response.data));
+    }
+};
+
+export const deleteUser = id => {
+    return async dispatch => {
+        try {
+            await axiosApi.delete('/users/' + id);
+            const response = await axiosApi.get('/users');
+            const users = response.data;
+            dispatch(fetchUsersSuccess(users));
+        } catch (e) {
+            dispatch(fetchUsersFailure(e));
+        }
+    }
+};
+
+export const editUser = (id, newUser) => {
+    return async dispatch => {
+        try {
+            dispatch(fetchUsersRequest());
+            await axiosApi.put('/users/' + id, newUser);
+            dispatch(editUserSuccess(id, newUser));
+        } catch (e) {
+            dispatch(fetchUsersFailure(e));
+        }
+    }
+}
+
---
+++ frontend/store/reducers/usersReducer.js
@@ -0,0 +1,39 @@
+import {
+    EDIT_USER_SUCCESS,
+    FETCH_SINGLE_USER_SUCCESS,
+    FETCH_USERS_FAILURE,
+    FETCH_USERS_REQUEST,
+    FETCH_USERS_SUCCESS
+} from "../actions/actionTypes";
+
+const initialState = {
+    users: [],
+    user: null,
+    error: null
+};
+
+const usersReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_USERS_SUCCESS:
+            return {...state, users: action.users};
+        case FETCH_SINGLE_USER_SUCCESS:
+            return {...state, user: action.user};
+        case FETCH_USERS_FAILURE:
+            return {...state, error: action.error};
+        case FETCH_USERS_REQUEST:
+            return {...state, error: null};
+        case EDIT_USER_SUCCESS:
+            return {
+                ...state,
+                users: {
+                    ...state.users,
+                    [action.id]: action.newUser
+                }
+            };
+        default:
+            return state;
+    }
+};
+
+
+export default usersReducer();
