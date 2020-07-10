e3b9bba5237b10c7a68d4a19004b9aed839745ef
Daria Chernovol <fabulus.kg.dk@gmail.com>
2020-07-10 16:12:20 +0600

    Added articles actions and reducer

---
+++ frontend/store/actions/articlesActions.js
@@ -0,0 +1,62 @@
+import axiosApi from "../../axiosApi";
+import {
+    EDIT_ARTICLE_SUCCESS, FETCH_ARTICLES_FAILURE, FETCH_ARTICLES_REQUEST,
+    FETCH_ARTICLES_SUCCESS,
+    FETCH_SINGLE_ARTICLE_SUCCESS,
+    POST_ARTICLE_SUCCESS
+} from "./actionTypes";
+
+
+export const fetchArticlesSuccess = articles => ({type: FETCH_ARTICLES_SUCCESS, articles});
+export const postArticleSuccess = () => ({type: POST_ARTICLE_SUCCESS});
+export const editArticleSuccess = (id, newArticle) => ({type: EDIT_ARTICLE_SUCCESS, id, newArticle});
+export const fetchSingleArticleSuccess = article => ({type: FETCH_SINGLE_ARTICLE_SUCCESS, article});
+export const fetchArticlesFailure = error => ({type: FETCH_ARTICLES_FAILURE, error});
+export const fetchArticlesRequest = () => ({type: FETCH_ARTICLES_REQUEST});
+
+export const fetchArticles = () => {
+    return async dispatch => {
+        const response = await axiosApi.get('/articles');
+        dispatch(fetchArticlesSuccess(response.data));
+    }
+};
+
+export const postArticle = article => {
+    return async dispatch => {
+        await axiosApi.post('/articles', article);
+        dispatch(postArticleSuccess());
+    }
+};
+
+export const fetchSingleArticle = id => {
+    return async (dispatch) => {
+        const response = await axiosApi.get('/articles/' + id);
+        dispatch(fetchSingleArticleSuccess(response.data));
+    }
+};
+
+export const deleteArticle = id => {
+    return async dispatch => {
+        try {
+            await axiosApi.delete('/articles/' + id);
+            const response = await axiosApi.get('/articles');
+            const articles = response.data;
+            dispatch(fetchArticlesSuccess(articles));
+        } catch (e) {
+            dispatch(fetchArticlesFailure(e));
+        }
+    }
+};
+
+export const editArticle = (id, newArticle) => {
+    return async dispatch => {
+        try {
+            dispatch(fetchArticlesRequest());
+            await axiosApi.put('/articles/' + id, newArticle);
+            dispatch(editArticleSuccess(id, newArticle));
+        } catch (e) {
+            dispatch(fetchArticlesFailure(e));
+        }
+    }
+}
+
---
+++ frontend/store/reducers/articlesReducer.js
@@ -0,0 +1,39 @@
+import {
+    EDIT_ARTICLE_SUCCESS,
+    FETCH_ARTICLES_FAILURE,
+    FETCH_ARTICLES_REQUEST,
+    FETCH_ARTICLES_SUCCESS,
+    FETCH_SINGLE_ARTICLE_SUCCESS
+} from "../actions/actionTypes";
+
+const initialState = {
+    articles: [],
+    article: null,
+    error: null
+};
+
+const articlesReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_ARTICLES_SUCCESS:
+            return {...state, users: action.articles};
+        case FETCH_SINGLE_ARTICLE_SUCCESS:
+            return {...state, article: action.article};
+        case FETCH_ARTICLES_FAILURE:
+            return {...state, error: action.error};
+        case FETCH_ARTICLES_REQUEST:
+            return {...state, error: null};
+        case EDIT_ARTICLE_SUCCESS:
+            return {
+                ...state,
+                articles: {
+                    ...state.articles,
+                    [action.id]: action.newArticle
+                }
+            };
+        default:
+            return state;
+    }
+};
+
+
+export default articlesReducer();
